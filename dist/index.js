c46a3b066f8b35394af6962003559e6d050407af
i.kologrivov <ikologrivov@corp.vk.com>
2023-03-03 18:19:13 +0300

    test

--- dist/index.js
+++ dist/index.js
@@ -17443,7 +17443,7 @@ const axios = __nccwpck_require__(1441);
 async function run() {
   try {
     const data = {
-      token: process.env.API_TOKEN,
+      access_token: process.env.API_TOKEN,
       product_id: Number(core.getInput('product_id')),
       version_id: -1,
       assembly_id: 0,
@@ -17451,13 +17451,13 @@ async function run() {
       release_notes: core.getInput('release_notes'),
       visible: core.getBooleanInput('visible') ? 1 : 0,
       set_rft: core.getBooleanInput('set_rft') ? 1 : 0,
-      v: 5.130
+      v: 5.135
     };
 
     const params_string = JSON.stringify(data, undefined, 2);
     console.log(`params: ${params_string}`);
 
-    const response = await axios.get('https://api.who2who.online/method/bugtracker.saveProductVersion', {params: data});
+    const response = await axios.get('https://api.vk.com/method/bugtracker.saveProductVersion', {params: data});
     if (response.data.error) {
       throw new Error(response.data.error.error_msg);
     }
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 async function run() {
   try {
     const data = {
-      token: process.env.API_TOKEN,
+      access_token: process.env.API_TOKEN,
       product_id: Number(core.getInput('product_id')),
       version_id: -1,
       assembly_id: 0,
@@ -13,13 +13,13 @@ async function run() {
       release_notes: core.getInput('release_notes'),
       visible: core.getBooleanInput('visible') ? 1 : 0,
       set_rft: core.getBooleanInput('set_rft') ? 1 : 0,
-      v: 5.130
+      v: 5.135
     };
 
     const params_string = JSON.stringify(data, undefined, 2);
     console.log(`params: ${params_string}`);
 
-    const response = await axios.get('https://api.who2who.online/method/bugtracker.saveProductVersion', {params: data});
+    const response = await axios.get('https://api.vk.com/method/bugtracker.saveProductVersion', {params: data});
     if (response.data.error) {
       throw new Error(response.data.error.error_msg);
     }
